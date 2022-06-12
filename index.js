7b6c572f101c003708325e34c9dcf8232d209348
erikkodnar <erik@gskinner.com>
2022-06-12 14:47:35 -0600

    changed server implementation

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const socketio = require('socket.io')
-const path = require('path')
-const logger = require('morgan')
+const { Server } = require("socket.io");
+const path = require('path');
+const logger = require('morgan');
 var cors = require('cors');
+require('dotenv').config();
+
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(cors())
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let http = require("http").Server(app);
-// set up socket.io and bind it to our
-// http server.
-let io = require("socket.io")(http);
 
+
+server.listen(process.env.IO_PORT);
+console.log(process.env.IO_PORT)
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./client/index.html"));
 });
@@ -26,7 +31,7 @@ io.on("connection", function(socket) {
   console.log("a user connected");
   socket.on("message", (message) => {
     console.log(message)
-  })
+  });
 });
 
 module.exports = {app};
